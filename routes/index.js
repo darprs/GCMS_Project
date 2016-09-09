cd47ea6af663cac9d7e292caacd776f8e3d80bb1
master <pavel.jv@gmail.com>
2016-09-09 23:33:12 +0300

    all files for farm servies served by farm.js and accessable using: http://localhost:8080/farm/{url for router}

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var generateName = require('sillyname');
 
-
-
-
-
-
-
-
-
-router.get('/local/:itemUID/sp/:parentUID', function(req, res){
-    test_item(req.params.itemUID ,req.params.parentUID,
-        function(uid, p_uid ,result){
-            if (!result)
-                {  res.send("item "  + uid + " not exist.");}
-            else
-                {
-                    test_item(p_uid,uid,
-                        function (p_uid, uid ,result) {
-                            if (!result)
-                            {  res.send("item "  + p_uid + " not exist.");}
-                            else {
-                                console.log("Changing parent of ", uid, " to ", p_uid);
-                                res.send(("Changing parent of "+ uid + " to " + p_uid));
-                            }
-                    });
-                }
-
-        });
-});
-
-
-
-
+//all files for farm servies served by farm.js and accessable using: http://localhost:8080/farm/
 
 module.exports = router;
