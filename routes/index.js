4c6f856227ae51306462502a76ac0eb08f40e29e
darprs <darprs@gmail.com>
2016-09-10 17:55:56 +0300

    ...

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 //all files for farm services served by farm.js and accessable using: http://localhost:8080/farm/
 
 
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+      res.render('index', { title: 'Express' });
+});
 
 
 module.exports = router;
