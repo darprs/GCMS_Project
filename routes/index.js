607fa04fc69f717d27bb339c855121917d0bcfa2
darprs <darprs@gmail.com>
2016-09-10 20:43:01 +0300

    changed: index.js -> added get_all index.jade -> changed view

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,64 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+/**************************************************************************/
+/* Daria Test */
+
+router.get('/get_all', function(req, res)
+{
+      ff.get_all(function (all) {
+            res.send(all);
+      });
+});
+
+
+module.exports.get_all = function(callback)
+{
+      console.log('\n Daria testing in functions');
+
+      Item.aggregate
+      ( [ {$sort: {"item_uid":1,"item_version":-1}},
+                { $group : { _id : "$item_uid" ,
+                      item_uid:{$first:"$item_uid"},
+                      item_version: {$first:"$item_version"},
+                      item_name: {$first:"$item_name"},
+                      item_parent_uid:{$first:"$item_parent_uid"}
+                }
+                },{$sort: {"_id":1}}
+          ],
+          function(err, all) {
+                if (all.length == 0)
+                {
+                      console.log('\n Daria - ERORR!!!');
+                      ff.get_all(callback);
+                }
+                else {
+                      console.log('\n Daria - All found');
+                      callback(all);
+                }
+          });
+};
+
+/*
+ module.exports.get_all = function(callback)
+ {
+ console.log('\n Daria testing in functions');
+
+ Item.find( function(err, all) {
+ if (all.length == 0)
+ {
+ console.log('\n Daria - ERORR!!!');
+ ff.get_all(callback);
+ }
+ else {
+ console.log('\n Daria - All found');
+ callback(all);
+ }
+ });
+ };
+
+ */
+
+
 module.exports = router;
