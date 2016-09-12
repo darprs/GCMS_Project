099ff8ecb72fc08c5dc7ebb3ac9f30e8cc9bec27
darprs <!Qazxcde32>
2016-09-12 22:27:59 +0300

    get all working

--- public/client.js
+++ public/client.js
@@ -3,51 +3,38 @@
  */
 
 $("#Entry_Btn_Login").click(function(){
-    $.get("/ISR10000000/gc", function(data, status){
+    $.get("/get_all", function(data, status){
 
-        var json_string = JSON.stringify(data);
+        //var json_string = JSON.stringify(data);
 
-        var tree = '';
+        var tree = '[ ';
 
         for(var i in data)
         {
-            tree += "{ 'id' : '" + data[i].item_uid + "' , 'parent' :  '";
+            tree += '{ "id" : "' + data[i].item_uid + '" , "parent" :  "';
 
             if(data[i].item_parent_uid == 'ISR10000000')
             {
-                tree += "#' , 'text' : '" + data[i].item_name + "' } ,";
+                tree += '#" , "text" : "' + data[i].item_name + '" } ,';
             }
             else
             {
-                tree += data[i].item_parent_uid + "' , 'text' : '" + data[i].item_name + "' } ,";
+                tree += data[i].item_parent_uid + '" , "text" : "' + data[i].item_name + '" } ,';
 
             }
         }
 
-        tree--;
+        var len = tree.length;
+        tree = tree.substring(0, len-1);
+        tree += ' ]';
 
-        //alert(JSON.stringify(tree));
+        console.log(tree);
 
+        res_json = JSON.parse(tree);
 
-        $('#MyTree').jstree({ 'core' : {'data': [
-            { 'id' : 'ISR10000001' , 'parent' :  '#' , 'text' : 'Fringewatcher Stone' } ,
-            { 'id' : 'ISR10000014' , 'parent' :  '#' , 'text' : 'Vinegriffin Back' } ,
-            { 'id' : 'ISR10000014' , 'parent' :  '#' , 'text' : 'Spangleheron Chest' } ,
-            { 'id' : 'ISR10000014' , 'parent' :  '#' , 'text' : 'Cookiellama Talon' } ,
-            { 'id' : 'ISR10000014' , 'parent' :  '#' , 'text' : 'Firelf Friend' } ,
-            { 'id' : 'ISR10000013' , 'parent' :  '#' , 'text' : 'Swiftgoose Minnow' } ,
-            { 'id' : 'ISR10000012' , 'parent' :  '#' , 'text' : 'Hickorybite Hound' } ,
-            { 'id' : 'ISR10000011' , 'parent' :  'ISR10000001' , 'text' : 'Junglesoarer Zebra' } ,
-            { 'id' : 'ISR10000010' , 'parent' :  'ISR10000014' , 'text' : 'Rattlefoe Lynx' } ,
-            { 'id' : 'ISR10000009' , 'parent' :  'ISR10000014' , 'text' : 'Rippleswoop Lifter' } ,
-            { 'id' : 'ISR10000008' , 'parent' :  'ISR10000014' , 'text' : 'Glenbird Stallion' } ,
-            { 'id' : 'ISR10000007' , 'parent' :  'ISR10000009' , 'text' : 'Rainbowking Whip' } ,
-            { 'id' : 'ISR10000006' , 'parent' :  'ISR10000009' , 'text' : 'Quivermole Knave' } ,
-            { 'id' : 'ISR10000005' , 'parent' :  'ISR10000006' , 'text' : 'Baldcrest Finger' } ,
-            { 'id' : 'ISR10000004' , 'parent' :  'ISR10000006' , 'text' : 'Bushwarrior Toucan' } ,
-            { 'id' : 'ISR10000003' , 'parent' :  'ISR10000006' , 'text' : 'Legendelk Sight' } ,
-            { 'id' : 'ISR10000002' , 'parent' :  'ISR10000006' , 'text' : 'Vineraptor Condor' }
-            ] } });
+        $('#MyTree').jstree({ 'core' : {'data': res_json } });
+
+        $("#Entry_Btn_Login").hide();
     });
 });
 
